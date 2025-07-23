e46f3d1366689fa651a2b5d59dedb0cdb7e5efcc
truongpm234 <ghoul1645@gmail.com>
2025-07-23 21:26:11 +0700

    add scene select level or auto mode

---
+++ Assets/Scripts/LevelSelectorUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectorUI : MonoBehaviour
+{
+    public void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void StartAutoMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
