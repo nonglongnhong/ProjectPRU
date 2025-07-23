92686e95c2c9f41d14f9933b6fc87acef75cc06a
truongpm234 <ghoul1645@gmail.com>
2025-07-23 14:00:28 +0700

    replace prefab with another asset and add 4 scene template

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -3,8 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    [SerializeField] private string sceneToLoad = "SampleScene";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayGame();
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
