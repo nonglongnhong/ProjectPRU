619c0a6acc9fa3ae1180255a73259f7bd8da442d
NongLongNhong <long041103@gmail.com>
2025-07-24 14:11:44 +0700

    add sound and change structure of scene 5 and scene 6

---
+++ Assets/Scripts/AutoStart.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class AutoStart
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void Init()
+    {
+        Application.runInBackground = true;
+        Object.DontDestroyOnLoad(new GameObject("InputStarter", typeof(InputStarter)));
+    }
+
+    private class InputStarter : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (SceneManager.GetActiveScene().name == "MainMenu" && Input.anyKeyDown)
+            {
+                SceneManager.LoadScene("LevelSelector");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad = "LevelSelector";
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("Any key pressed");
+            PlayGame();
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
